49ba8e40a6bfe4f4f9abd8268803dcb4b3a2903c
Amin Zena <eminzeyne16@gmail.com>
2025-10-16 12:48:51 +0300

    Delete 5.Hafta directory

--- 5.Hafta/alisveris/ust/ustresim.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.thumbs img').forEach(img => {
-    img.addEventListener('mouseover', () => {
-      const bigSrc = img.getAttribute('data-big');
-      document.getElementById('bigImage').innerHTML = `<img src="${bigSrc}" alt="">`;
-    });
-  });
-  
